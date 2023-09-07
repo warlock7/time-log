045e16b3ae7520ee36d3b50471ed16a19a0f8e6a
George Mathew Kurian <georgemathewkurian@gmail.com>
2023-09-07 23:48:25 +0200

    feat: add logs in state

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,8 +8,12 @@ type ILog = {
 
 interface LogState {
   log: ILog;
+  logs: {
+    [key: string]: ILog;
+  };
   setLog: (log: ILog) => void;
   setDate: (date: Date) => void;
+  setLogs: (log: ILog, key: string) => void;
 }
 
 export const useLogStore = create<LogState>()((set) => ({
@@ -18,9 +22,12 @@ export const useLogStore = create<LogState>()((set) => ({
     hour: 0,
     date: new Date(),
   },
+  logs: {},
   setDate: (date: Date) => set((state) => ({ log: { ...state.log, date } })),
   setLog: (log: ILog) =>
     set((state) => ({
       log: { ...state.log, ...log },
     })),
+  setLogs: (log: ILog, key: string) =>
+    set((state) => ({ logs: { ...state.logs, [key]: log } })),
 }));
