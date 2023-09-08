93079f1d82c8e5aeffe889faef306431ba00265e
George Mathew Kurian <georgemathewkurian@gmail.com>
2023-09-08 18:57:26 +0200

    feat: handle logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,30 @@
+'use client';
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { usePathname, useRouter } from 'next/navigation';
+
 import { IoMdTimer } from 'react-icons/io';
 import { Button } from './ui/button';
 
 export default function Navbar() {
+  const router = useRouter();
+  const path = usePathname();
+  const supabase = createClientComponentClient();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  const isAuthPage = path === '/auth';
+
   return (
     <div className="flex items-center justify-between  ">
       <div className="flex items-center text-2xl">
         <IoMdTimer />
         <h1>Time</h1>
       </div>
-      <Button>Logout</Button>
+      {!isAuthPage && <Button onClick={handleLogout}>Logout</Button>}
     </div>
   );
 }
